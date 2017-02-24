c65fdfd290ef611beea486ab5d7489d707fd18a4
bardhu <ar.thr@hotmail.com>
2017-02-24 16:28:29 -0300

    Add files via upload

--- Monolit-Nayaraujo_arq/monolit/php/contact.php
+++ Monolit-Nayaraujo_arq/monolit/php/contact.php
@@ -38,7 +38,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "dimagalishev@gmail.com";
+$address = "contato@nayaraujo.arq.br";
 
 
 // Configuration option.
@@ -46,16 +46,16 @@ $address = "dimagalishev@gmail.com";
 
 // Example, $e_subject = '$name . ' has contacted you via Your Website.';
 
-$e_subject = 'You\'ve been contacted by ' . $name . '.';
+$e_subject = 'Você foi contatado por ' . $name . '.';
 
 
 // Configuration option.
 // You can change this if you feel that you need to.
 // Developers, you may wish to add more fields to the form, in which case you must be sure to add them here.
 
-$e_body = "You have been contacted by: $name" . PHP_EOL . PHP_EOL;
+$e_body = "Você foi contatado por: $name" . PHP_EOL . PHP_EOL;
 $e_reply = "E-mail: $email\r\nPhone: $phone";
-$e_content = "Message:\r\n$comments" . PHP_EOL . PHP_EOL;
+$e_content = "Mensagem:\r\n$comments" . PHP_EOL . PHP_EOL;
 
 
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
